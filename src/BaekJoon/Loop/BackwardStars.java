36781fea28095bd2491892cbecd84ef28ea3e3eb
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-06 22:26:37 +0900

    - 25.01.07 백준 알고리즘 반복문 8~10문제

--- src/BaekJoon/Loop/BackwardStars.java
+++ src/BaekJoon/Loop/BackwardStars.java
@@ -1,11 +1,22 @@
+package BaekJoon.Loop;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.StringReader;
 
-public class Main {
+/**
+ * 백준 2439
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ *
+ * 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+ *
+ * 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+ *
+ * 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+public class BackwardStars {
 
-    public static void main(String[] args) throws IOException {
+    public void oldMemories () throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int totalCount = Integer.parseInt(bf.readLine());
@@ -26,5 +37,4 @@ public class Main {
         System.out.println(sb);
         bf.close();
     }
-
 }
