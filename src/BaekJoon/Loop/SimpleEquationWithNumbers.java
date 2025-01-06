128109982d48ee9407acfdc35e351050b88c1421
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-06 21:56:15 +0900

    - 25.01.07 백준 알고리즘 반복문 5~7문제

--- src/BaekJoon/Loop/SimpleEquationWithNumbers.java
+++ src/BaekJoon/Loop/SimpleEquationWithNumbers.java
@@ -1,11 +1,20 @@
+package BaekJoon.Loop;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 백준 11021
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ * 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+ */
+public class SimpleEquationWithNumbers {
 
-    public static void main(String[] args) throws IOException {
+    public void moreFaster() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String totalCountString = bf.readLine();
@@ -30,5 +39,4 @@ public class Main {
         System.out.println(sb);
         bf.close();
     }
-
 }
