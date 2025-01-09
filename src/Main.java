530efa683f88a21982b6b7f089750a5c29dd1687
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-09 21:34:09 +0900

    - 25.01.09 백준 알고리즘 1차 배열 10번 문제 (총 1문제)

--- src/Main.java
+++ src/Main.java
@@ -7,50 +7,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int totalSubjectCount = Integer.parseInt(bf.readLine());
+
+        double[] subjectScores = new double[totalSubjectCount];
+
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int totalBaskets = Integer.parseInt(st.nextToken());
-        int totalInput = Integer.parseInt(st.nextToken());
 
-        int[] baskets = new int[totalBaskets];
+        double topScore = 0;
 
-        for (int i = 0; i < totalBaskets; i++) {
-            baskets[i] = i + 1;
-        }
-
-        for (int i = 0; i < totalInput; i++) {
-            StringTokenizer st2 = new StringTokenizer(bf.readLine());
-            int firstBasket = Integer.parseInt(st2.nextToken());
-            int lastBasket = Integer.parseInt(st2.nextToken());
-
-            int temp1 = baskets[firstBasket - 1];
-            baskets[firstBasket - 1] = baskets[lastBasket - 1];
-            baskets[lastBasket - 1] = temp1;
-            int remainingCount = lastBasket - firstBasket - 1;
-
-            if (remainingCount > 0) {
-                remainingCount = remainingCount / 2;
-                firstBasket += 1;
-                lastBasket -= 1;
-
-                for (int j = 0; j < remainingCount; j++) {
-                    int temp = baskets[firstBasket - 1];
-                    baskets[firstBasket - 1] = baskets[lastBasket - 1];
-                    baskets[lastBasket - 1] = temp;
-                    firstBasket += 1;
-                    lastBasket -= 1;
-                }
-
+        for(int i = 0 ; i < totalSubjectCount ; i++) {
+            subjectScores[i] = Integer.parseInt(st.nextToken());
+            if(subjectScores[i] > topScore) {
+                topScore = subjectScores[i];
             }
         }
 
-        bf.close();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < totalBaskets; i++) {
-            sb.append(baskets[i]);
-            sb.append(" ");
+        double totalSum = 0;
+        for (int i = 0 ; i < totalSubjectCount ; i++) {
+            totalSum += subjectScores[i]/topScore*100;
         }
 
-        System.out.println(sb);
+        double finalValue = totalSum / totalSubjectCount;
+
+        System.out.println(finalValue);
     }
 }
