7a17d62bd12ba096cf1e51ad627172fe7021f6d9
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-10 21:34:00 +0900

    - 25.01.10 백준 알고리즘 1차 문자열 9~11번 문제 (총 3문제)
    Took 57 minutes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String firstLine = bf.readLine();
+        StringBuilder sb = new StringBuilder();
 
-        String sentence = bf.readLine();
-        StringTokenizer st = new StringTokenizer(sentence,  " ");
-        bf.close();
-        int totalWords = 0;
-        while(st.hasMoreTokens()) {
-            totalWords += 1;
-            st.nextToken();
+        while(Optional.ofNullable(firstLine).isPresent()) {
+            sb.append(firstLine);
+            sb.append(System.lineSeparator());
+            firstLine = bf.readLine();
         }
+        bf.close();
 
-        System.out.println(totalWords);
+        System.out.println(sb);
     }
 }
