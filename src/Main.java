fa7f608a870a42923fcdda59f92e20cc561f2372
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-18 23:06:38 +0900

    - 25.01.18 백준 알고리즘 2차원 배열 1~3 (3문제)
    Took 50 minutes

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] subjects = new String[20];
-        Double[] credits = new Double[20];
-        String[] actualScore = new String[20];
-        Double[] scores = new Double[20];
-        int totalSubject = 20;
-        double totalCredit = 0.0;
+        char[][] totalMatrix = new char[15][15];
+        StringBuilder sb = new StringBuilder();
 
-        String[] scoreBoard = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+        for (int i = 0; i < 5; i++) {
+            String firstLine = bf.readLine();
 
-        double averageScore = 0.0;
-
-        for (int i = 0; i < 20; i++) {
-            String line = bf.readLine();
-            StringTokenizer st = new StringTokenizer(line, " ");
-            subjects[i] = st.nextToken();
-            credits[i] = Double.parseDouble(st.nextToken());
-            actualScore[i] = st.nextToken();
-            switch (actualScore[i]) {
-                case "A+":
-                    scores[i] = 4.5;
-                    break;
-                case "A0":
-                    scores[i] = 4.0;
-                    break;
-                case "B+":
-                    scores[i] = 3.5;
-                    break;
-                case "B0":
-                    scores[i] = 3.0;
-                    break;
-                case "C+":
-                    scores[i] = 2.5;
-                    break;
-                case "C0":
-                    scores[i] = 2.0;
-                    break;
-                case "D+":
-                    scores[i] = 1.5;
-                    break;
-                case "D0":
-                    scores[i] = 1.0;
-                    break;
-                case "F":
-                case "P":
-                    scores[i] = 0.0;
-                    break;
-            }
-            if (!"P".equals(actualScore[i])) {
-                averageScore += credits[i] * scores[i];
-                totalCredit += credits[i];
-            } else {
-                totalSubject--;
+            for (int j = 0; j < firstLine.length(); j++) {
+                totalMatrix[i][j] = firstLine.charAt(j);
             }
         }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (totalMatrix[j][i] != 0) {
+                    sb.append(totalMatrix[j][i]);
+                }
+            }
+        }
+
+        System.out.println(sb);
         bf.close();
-        System.out.println(averageScore / totalCredit);
     }
 }
