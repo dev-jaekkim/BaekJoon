9293daf8a323bec2e63a2736e8a4f78e31fb3670
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-18 22:16:08 +0900

    - 25.01.18 백준 알고리즘 심화 1 7~8 (2문제)
    Took 2 hours 12 minutes

--- src/BaekJoon/advanced/stepOne/YourAverage.java
+++ src/BaekJoon/advanced/stepOne/YourAverage.java
@@ -1,11 +1,38 @@
+package BaekJoon.advanced.stepOne;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 백준 25206
+ * 인하대학교 컴퓨터공학과를 졸업하기 위해서는, 전공평점이 3.3 이상이거나 졸업고사를 통과해야 한다.
+ * 그런데 아뿔싸, 치훈이는 깜빡하고 졸업고사를 응시하지 않았다는 사실을 깨달았다!
+ * <p>
+ * 치훈이의 전공평점을 계산해주는 프로그램을 작성해보자.
+ * <p>
+ * 전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
+ * <p>
+ * 인하대학교 컴퓨터공학과의 등급에 따른 과목평점은 다음 표와 같다.
+ * <p>
+ * A+	4.5
+ * A0	4.0
+ * B+	3.5
+ * B0	3.0
+ * C+	2.5
+ * C0	2.0
+ * D+	1.5
+ * D0	1.0
+ * F	0.0
+ * <p>
+ * P/F 과목의 경우 등급이 P또는 F로 표시되는데, 등급이 P인 과목은 계산에서 제외해야 한다.
+ * <p>
+ * 과연 치훈이는 무사히 졸업할 수 있을까?
+ * 20줄에 걸쳐 치훈이가 수강한 전공과목의 과목명, 학점, 등급이 공백으로 구분되어 주어진다.
+ */
+public class YourAverage {
 
-    public static void main(String[] args) throws IOException {
+    public void canYouGraduate() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String[] subjects = new String[20];
@@ -14,8 +41,6 @@ public class Main {
         Double[] scores = new Double[20];
         int totalSubject = 20;
         double totalCredit = 0.0;
-
-        String[] scoreBoard = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
 
         double averageScore = 0.0;
 
@@ -56,12 +81,22 @@ public class Main {
                     break;
             }
             if (!"P".equals(actualScore[i])) {
+                System.out.println("subjects = " + subjects[i]);
+                System.out.println("actualScore = " + actualScore[i]);
+                System.out.println("credits = " + credits[i]);
+                System.out.println("scores = " + scores[i]);
+                System.out.println("합산 전 averageScore = " + averageScore);
                 averageScore += credits[i] * scores[i];
+                System.out.println("합산 후 averageScore = " + averageScore);
+                System.out.println("합산 할 과목 수 : " + totalSubject);
                 totalCredit += credits[i];
             } else {
                 totalSubject--;
             }
         }
+        System.out.println("averageScore = " + averageScore);
+        System.out.println("totalSubject = " + totalSubject);
+
         bf.close();
         System.out.println(averageScore / totalCredit);
     }
