b24ccc9b055008d26dc743b1ca8e1e7ae6ad08ce
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-18 17:15:19 +0900

    - 25.01.10 백준 알고리즘 심화 1 1~2(2문제)
    Took 1 hour 40 minutes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String firstLine = bf.readLine();
-        StringBuilder sb = new StringBuilder();
 
-        while(Optional.ofNullable(firstLine).isPresent()) {
-            sb.append(firstLine);
-            sb.append(System.lineSeparator());
-            firstLine = bf.readLine();
-        }
+        int inputNumber = Integer.parseInt(bf.readLine());
         bf.close();
 
-        System.out.println(sb);
+        int totalLine = inputNumber * 2 - 1;
+        int totalBlank = totalLine / 2;
+        int firstStar = 1;
+
+        StringBuilder sb = new StringBuilder();
+        boolean goingDownWards = true;
+
+        for (int i = 0; i < totalLine; i++) {
+
+            if (goingDownWards) {
+                for (int j = 0; j < totalBlank; j++) {
+                    sb.append(" ");
+                }
+                for (int k = 0; k < firstStar; k++) {
+                    sb.append("*");
+                }
+
+                    totalBlank -= 1;
+                    firstStar += 2;
+                    sb.append(System.lineSeparator());
+
+                if (firstStar > totalLine) {
+                    totalBlank += 1;
+                    firstStar -= 2;
+                    goingDownWards = false;
+                }
+            }
+
+            if (!goingDownWards && firstStar > 1) {
+                totalBlank += 1;
+                firstStar -= 2;
+
+                for (int j = 0; j < totalBlank; j++) {
+                    sb.append(" ");
+                }
+                for (int k = 0; k < firstStar; k++) {
+                    sb.append("*");
+                }
+
+                if(firstStar > 1){
+                    sb.append(System.lineSeparator());
+                }
+
+            }
+        }
+        System.out.print(sb);
     }
 }
