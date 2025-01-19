da59ecbcf787b9e487c01c04e25446bebe89cfdd
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-19 12:24:22 +0900

    - 25.01.18 백준 알고리즘 일반수학1 2번
    Took 38 minutes

--- src/BaekJoon/maths/ChangeFormationBackwards.java
+++ src/BaekJoon/maths/ChangeFormationBackwards.java
@@ -1,11 +1,20 @@
+package BaekJoon.maths;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 백준 11005
+ * 10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+ *
+ * 10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+ *
+ * A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+ */
+public class ChangeFormationBackwards {
 
-    public static void main(String[] args) throws IOException {
+    public void backwards() throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String input = bf.readLine();
@@ -18,11 +27,11 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
 
-        while(NumberToSolve >= formation) {
+        while(NumberToSolve > formation) {
 
             char c = 'a';
 
-            if(NumberToSolve % formation >= 10 ) {
+            if(NumberToSolve % formation > 10 ) {
                 c = (char) ((NumberToSolve % formation) + 55);
             } else {
                 c = (char) ((NumberToSolve % formation) + 48);
@@ -31,13 +40,6 @@ public class Main {
             NumberToSolve = NumberToSolve / formation;
             sb.insert(0, c);
         }
-
-        if(NumberToSolve >= 10 ) {
-            sb.insert(0,(char) ((NumberToSolve + 55)));
-        } else {
-            sb.insert(0,(char) ((NumberToSolve + 48)));
-        }
-
         System.out.println(sb);
     }
 }
