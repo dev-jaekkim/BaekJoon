89c85188bf04fd5a971fa69672fa06bb6775f62c
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-19 13:59:27 +0900

    - 25.01.19 클래스 2 - 백준 1978
    Took 17 minutes

--- src/BaekJoon/solved/ac/classes/class2/PrimeNumbers.java
+++ src/BaekJoon/solved/ac/classes/class2/PrimeNumbers.java
@@ -1,11 +1,15 @@
+package BaekJoon.solved.ac.classes.class2;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+/**
+ * 백준 1978
+ * 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+ */
+public class PrimeNumbers {
+    public void findThePrime() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int inputNumCount = Integer.parseInt(bf.readLine());
         int resultCount = 0;
@@ -17,8 +21,11 @@ public class Main {
         for (int i = 0; i < inputNumCount; i++) {
             int areYouPrime = Integer.parseInt(st.nextToken());
             boolean primeCheck = true;
+            System.out.println("areYouPrime = " + areYouPrime);
             for (int j = 2; j < areYouPrime; j++) {
+                System.out.println("j = " + j + " areYouPrime = " + areYouPrime);
                 if (areYouPrime % j == 0) {
+                    System.out.println("primeCheck False value = " + areYouPrime + " j = " + j);
                     primeCheck = false;
                     break;
                 }
