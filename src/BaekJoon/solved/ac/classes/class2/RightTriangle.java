50a24e5855f6679ba64a2c4b59c6362376c7adb7
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-19 13:19:45 +0900

    - 25.01.19 클래스 2 - 백준 4153
    Took 39 minutes

--- src/BaekJoon/solved/ac/classes/class2/RightTriangle.java
+++ src/BaekJoon/solved/ac/classes/class2/RightTriangle.java
@@ -1,12 +1,18 @@
+package BaekJoon.solved.ac.classes.class2;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 백준 4153
+ * 과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다.
+ * 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+ */
+public class RightTriangle {
 
-    public static void main(String[] args) throws IOException {
+    public void pitagoras() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         StringBuilder sb = new StringBuilder();
@@ -39,4 +45,5 @@ public class Main {
 
         System.out.println(sb);
     }
+
 }
