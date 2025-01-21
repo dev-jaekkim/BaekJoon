f187d2b2044285b983940ae0dad5ccdfe056edce
jaekyung <dev.jaekyungkim@gmail.com>
2025-01-21 21:46:28 +0900

    - 25.01.21 클래스 2 - 백준 2775,  10988
    Took 1 hour 6 minutes

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,27 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int targetNumber = Integer.parseInt(bf.readLine());
-        int count = 1;
-        int range = 2;
-        if (targetNumber == 1) {
-            System.out.println("1");
-        } else {
-            while (range <= targetNumber) {
-                range = range + (6*count);
-                count++;
+        int totalCount = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < totalCount; i++) {
+            int level = Integer.parseInt(bf.readLine());
+            int roomNumber = Integer.parseInt(bf.readLine());
+            int[][] apartment = new int[level + 1][roomNumber + 1];
+
+            for (int j = 0; j < level + 1; j++) {
+                for (int k = 1; k < roomNumber + 1; k++) {
+                    if (j == 0) {
+                        apartment[j][k] = k;
+                    } else {
+                        apartment[j][k] = apartment[j - 1][k] + apartment[j][k - 1];
+                    }
+                }
             }
-            System.out.println(count);
+            sb.append(apartment[level][roomNumber]);
+            sb.append(System.lineSeparator());
         }
+        System.out.println(sb);
+        bf.close();
     }
 }
